c544aa95621574fef7147cad8276c3b919dc6bfb
Usama Malik <um339272@gmail.com>
2023-10-27 17:29:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+Shery.imageEffect("#back", {style: 5,config: {"a":{"value":2,"range":[0,30]},"b":{"value":-0.92,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.1111109678361086},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.83,"range":[0,10]},"metaball":{"value":0.23,"range":[0,2],"_gsap":{"id":3}},"discard_threshold":{"value":0.36,"range":[0,1]},"antialias_threshold":{"value":0.01,"range":[0,0.1]},"noise_height":{"value":0.18,"range":[0,2]},"noise_scale":{"value":5.79,"range":[0,100]}}, gooey: true,})
+
+
+
+
+
+
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function(elem){    
+var h1s = elem.querySelectorAll("h1");
+var index = 0;
+var animating = false;
+document.querySelector("#main").addEventListener("click", function(){
+    if(!animating){
+
+        animating = true;
+
+        gsap.to(h1s[index],{
+            top: '-=100%',
+            ease: Expo.easeInOut,
+            duration: 1,
+            onComplete: function(){
+                gsap.set(this._targets[0],{
+                    top: "100%"
+                });
+
+                animating = false;
+
+            },
+        })
+        index===h1s.length-1? (index = 0) : index++;
+        gsap.to(h1s[index],{
+            top: '-=100%',
+            ease: Expo.easeInOut,
+            duration: 1,
+        })  
+    }
+})
+})
+
+
